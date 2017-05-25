8d3b60c2efc31f03e9b207e44f39129d9537fb44
tico-tico <sergei.ivn@gmx.com>
2017-05-25 15:38:51 +0300

    debug message for setoption

--- sources/src/uci_options.cpp
+++ sources/src/uci_options.cpp
@@ -143,6 +143,10 @@ void ParseSetoption(const char *ptr) {
     for (int i = 0; name[i]; i++)   // make `name` lowercase
         name[i] = tolower(name[i]); // we can't lowercase `value` 'coz paths are case-sensitive on linux
 
+#ifndef NDEBUG
+    printf( "(debug) setoption name:  '%s' value: '%s'\n", name, value );
+#endif
+
     if (strcmp(name, "hash") == 0)                                           {
         AllocTrans(atoi(value));
 #ifdef USE_THREADS
