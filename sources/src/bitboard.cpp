8b6a7f9d55348edecae6ddc6ec60beebb965610b
tico-tico <sergei.ivn@gmx.com>
2017-06-28 15:08:04 +0300

    POPCNT support for x32 for Visual Studio

--- sources/src/bitboard.cpp
+++ sources/src/bitboard.cpp
@@ -107,13 +107,17 @@ int cBitBoard::PopCnt(U64 bb) {
     return popcnt_gcc(bb);
 }
 
-#elif defined(USE_MM_POPCNT) && defined(_M_AMD64)  // 64 bit windows
+#elif defined(USE_MM_POPCNT)
 #include <nmmintrin.h>
-
-int cBitBoard::PopCnt(U64 bb) {
-    return (int)_mm_popcnt_u64(bb);
-}
-
+    #ifndef _M_AMD64 // 32 bit windows
+        int cBitBoard::PopCnt(U64 bb) {
+            return _mm_popcnt_u32((unsigned int)(bb >> 32)) + _mm_popcnt_u32((unsigned int)(bb));
+        }
+    #else            // 64 bit windows
+        int cBitBoard::PopCnt(U64 bb) {
+            return _mm_popcnt_u64(bb);
+        }
+    #endif
 #else
 
 int cBitBoard::PopCnt(U64 bb) { // general purpose population count
