254ea59c24dd0e8697a2f963ad297b86bda43fd4
tico-tico <sergei.ivn@gmx.com>
2018-01-13 12:05:27 +0300

    search for VS install dir a little bit different

--- sources/buildme_vs2017.bat
+++ sources/buildme_vs2017.bat
@@ -4,7 +4,7 @@ setlocal
 
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
-for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -property installationPath`) do (
   set InstallDir=%%i
 )
 
